b77f69d4b0f14d1f7fbdea23e3ef1512b7ddad0e
suryyaa <suryastarone@gmail.com>
2026-01-05 10:52:22 +0530

    Dashbord Router Navigation added

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,5 +1,6 @@
 ﻿import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from '@angular/fire/auth';
 import { Component } from '@angular/core';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,7 +9,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.css'],
 })
 export class Login {
-  constructor(private auth: Auth) {}
+  
+  constructor(private auth: Auth, private router:Router) {}
 
   register(email: string, password: string) {
     createUserWithEmailAndPassword(this.auth, email, password)
@@ -25,7 +27,8 @@ export class Login {
       .then((userCredential) => {
         console.log('User exists & logged in:', userCredential.user);
         alert('Login successful');
-        window.location.href = '/dashboard';
+        // window.location.href = '/dashboard';
+        this.router.navigate(['/dashboard']);
       })
       .catch((error) => {
         if (error.code === 'auth/user-not-found') {
